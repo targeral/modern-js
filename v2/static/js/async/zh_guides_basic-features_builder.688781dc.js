cd2945d5123f2c59dca80a0afb9e6440ad8be1cb
targeral <2654955040@qq.com>
2023-01-30 06:49:24 +0000

    Deploying to gh-pages from @ targeral/modern-js@fe7e80d78253c41b9e2c532f02d8d1dfd2e9b8fa 🚀

--- v2/static/js/async/zh_guides_basic-features_builder.688781dc.js
+++ v2/static/js/async/zh_guides_basic-features_builder.688781dc.js
@@ -37,7 +37,7 @@ const toc = [{
   "depth": 2
 }];
 const title = `构建能力`;
-const content = "\"---\\ntitle: 构建能力\\nsidebar_position: 2\\n---\\n# 构建能力\\n\\n**Modern.js 的构建能力由 [Modern.js Builder](https://modernjs.dev/builder) 提供。**\\n\\nModern.js Builder 是 Modern.js 体系的核心组件之一，它是一个面向 Web 开发场景的构建引擎，可以脱离 Modern.js 被独立使用。Modern.js Builder 同时支持 webpack 和 rspack 等多种打包工具，默认情况下使用最成熟的 webpack 进行打包。\\n\\n## 构建架构\\n\\n从构建的角度看，Modern.js 分为三层架构，从上到下依次是：\\n\\n- 上层研发框架：Modern.js。\\n- 通用构建引擎：Modern.js Builder。\\n- 底层打包工具：webpack 和 rspack。\\n\\n<img src=\\\"https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/builder-layers-1117.png\\\" style={{ maxWidth: '540px' }} />\\n\\n## 构建文档\\n\\n由于 Modern.js Builder 是一个可独立使用的模块，我们为它提供了单独的文档，文档地址为：[modernjs.dev/builder](https://modernjs.dev/builder)。\\n\\n在该文档中，你可以了解到 Modern.js Builder 的详细介绍，同时也可以找到各个构建能力的完整使用指南。当你遇到构建方面的需求或问题时，建议你优先阅读 Modern.js Builder 文档来解决。\\n\\n## 构建配置\\n\\nModern.js 的配置继承自 Modern.js Builder，因此你可以在 Modern.js 中使用 Modern.js Builder 提供的所有构建配置。\\n\\n以 Modern.js Builder 的 `html.title` 配置项为例，你可以直接在 `modern.config.ts` 文件中使用该配置项，它会被自动传递给 Modern.js Builder。\\n\\n```ts title=\\\"modern.config.js\\\"\\nexport default defineConfig({\\n  html: {\\n    title: 'example',\\n  },\\n});\\n```\\n\\n关于构建配置的详细说明，请参考 [「Modern.js Builder - Builder 配置」](https://modernjs.dev/builder/guide/basic/builder-config.html)。\\n\\n## 构建能力\\n\\nModern.js Builder 提供了丰富的构建能力，包括 JavaScript 编译、CSS 编译、静态资源处理、代码热更新、代码压缩、TS 类型检查等几十种能力。\\n\\n我们推荐你阅读 [「Modern.js Builder - 功能导航」](https://modernjs.dev/builder/guide/features.html) 来了解 Modern.js Builder 提供的所有构建能力。\\n\"";
+const content = "\"---\\ntitle: 构建能力\\nsidebar_position: 2\\n---\\n# 构建能力\\n\\n**Modern.js 的构建能力由 [Modern.js Builder](https://modernjs.dev/builder) 提供。**\\n\\nModern.js Builder 是 Modern.js 体系的核心组件之一，它是一个面向 Web 开发场景的构建引擎，可以脱离 Modern.js 被独立使用。Modern.js Builder 同时支持 webpack 和 Rspack 等多种打包工具，默认情况下使用最成熟的 webpack 进行打包。\\n\\n## 构建架构\\n\\n从构建的角度看，Modern.js 分为三层架构，从上到下依次是：\\n\\n- 上层研发框架：Modern.js。\\n- 通用构建引擎：Modern.js Builder。\\n- 底层打包工具：webpack 和 Rspack。\\n\\n<img src=\\\"https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/builder-layers-1117.png\\\" style={{ maxWidth: '540px' }} />\\n\\n## 构建文档\\n\\n由于 Modern.js Builder 是一个可独立使用的模块，我们为它提供了单独的文档，文档地址为：[modernjs.dev/builder](https://modernjs.dev/builder)。\\n\\n在该文档中，你可以了解到 Modern.js Builder 的详细介绍，同时也可以找到各个构建能力的完整使用指南。当你遇到构建方面的需求或问题时，建议你优先阅读 Modern.js Builder 文档来解决。\\n\\n## 构建配置\\n\\nModern.js 的配置继承自 Modern.js Builder，因此你可以在 Modern.js 中使用 Modern.js Builder 提供的所有构建配置。\\n\\n以 Modern.js Builder 的 `html.title` 配置项为例，你可以直接在 `modern.config.ts` 文件中使用该配置项，它会被自动传递给 Modern.js Builder。\\n\\n```ts title=\\\"modern.config.js\\\"\\nexport default defineConfig({\\n  html: {\\n    title: 'example',\\n  },\\n});\\n```\\n\\n关于构建配置的详细说明，请参考 [「Modern.js Builder - Builder 配置」](https://modernjs.dev/builder/guide/basic/builder-config.html)。\\n\\n## 构建能力\\n\\nModern.js Builder 提供了丰富的构建能力，包括 JavaScript 编译、CSS 编译、静态资源处理、代码热更新、代码压缩、TS 类型检查等几十种能力。\\n\\n我们推荐你阅读 [「Modern.js Builder - 功能导航」](https://modernjs.dev/builder/guide/features.html) 来了解 Modern.js Builder 提供的所有构建能力。\\n\"";
 function _createMdxContent(props) {
   const _components = Object.assign({
     h1: "h1",
@@ -72,7 +72,7 @@ function _createMdxContent(props) {
         }), " 提供。"]
       })
     }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_components.p, {
-      children: "Modern.js Builder 是 Modern.js 体系的核心组件之一，它是一个面向 Web 开发场景的构建引擎，可以脱离 Modern.js 被独立使用。Modern.js Builder 同时支持 webpack 和 rspack 等多种打包工具，默认情况下使用最成熟的 webpack 进行打包。"
+      children: "Modern.js Builder 是 Modern.js 体系的核心组件之一，它是一个面向 Web 开发场景的构建引擎，可以脱离 Modern.js 被独立使用。Modern.js Builder 同时支持 webpack 和 Rspack 等多种打包工具，默认情况下使用最成熟的 webpack 进行打包。"
     }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_components.h2, {
       id: "构建架构",
       children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_components.a, {
@@ -89,7 +89,7 @@ function _createMdxContent(props) {
       }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_components.li, {
         children: "通用构建引擎：Modern.js Builder。"
       }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_components.li, {
-        children: "底层打包工具：webpack 和 rspack。"
+        children: "底层打包工具：webpack 和 Rspack。"
       }), "\n"]
     }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("img", {
       src: "https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/builder-layers-1117.png",
